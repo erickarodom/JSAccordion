7971496f61c43d984bc8ccec9816f85b49555fce
erickarodoom <erickarodom@gmail.com>
2024-02-23 15:41:13 -0800

    add script test

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const open = document.querySelectorAll('.accordion-title');
 const close = document.querySelectorAll('#accordion-toggle');
 const content = document.querySelectorAll('#accordionContainer');
+console.log(close);
 
-function openContent(){
+// function openContent(){
         
-        content.classList.add('toggle-on');
-}
-function closeContent(){
-        content.classList.remove('toggle-on');
-}
+//         content.classList.add('toggle-on');
+// }
+// function closeContent(){
+//         content.classList.remove('toggle-on');
+// }
 
-open.addEventListener('click', openContent);
-close.addEventListener('click', closeContent);
+// open.addEventListener('click', openContent);
+// close.addEventListener('click', closeContent);
