2965f994beffa8e1e7ead317f302677f08a53a51
erickarodoom <erickarodom@gmail.com>
2024-02-27 16:23:08 -0800

    modified hover styles of component

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const close = document.querySelector('#accordion-toggle');
 const content = document.querySelector('#accordionContainer');
 console.log(close);
 
-// function openContent(){
+function openContent(){
         
-//         content.classList.add('toggle-on');
-// }
-// function closeContent(){
-//         content.classList.remove('toggle-on');
-// }
+        content.classList.add('toggle-on');
+}
+function closeContent(){
+        content.classList.remove('toggle-on');
+}
 
-// open.addEventListener('click', openContent);
-// close.addEventListener('click', closeContent);
+open.addEventListener('click', openContent);
+close.addEventListener('click', closeContent);
