7e86729b234099c9806b499f872391df1b7c6f94
erickarodoom <erickarodom@gmail.com>
2024-02-27 22:30:38 -0800

    minor change

--- script.js
+++ script.js
@@ -13,3 +13,9 @@ for(let i = 0; i < toggle.length; i++){
         content.classList.remove('toggle-on');
     });
 }
+for(let i = 0; i < toggle.length; i++){
+    toggle[i].addEventListener('click', function(e){
+        let content = e.target.parentElement.nextElementSibling;
+        content.classList.remove('toggle-on');
+    });
+}
