5c73a9935859db3b86de1d1756cd93de52ed557f
erickarodoom <erickarodom@gmail.com>
2024-02-27 21:48:56 -0800

    added styling to main content div

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 const title = document.querySelectorAll('.accordion-title');
 const toggle = document.querySelectorAll('.accordion-toggle');
 
-// function openContent(){
-//         content.classList.add('toggle-on');
-// }
-// function closeContent(){
-//         content.classList.remove('toggle-on');
-// }
 for(let i = 0; i < title.length; i++){
     title[i].addEventListener('click', function(e){
         let content = e.target.parentElement.nextElementSibling;
