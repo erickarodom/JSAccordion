12069665f72a15034d87355f41735dbf06ff419c
erickarodoom <erickarodom@gmail.com>
2024-02-27 21:35:15 -0800

    added functionality to close accordian

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 const title = document.querySelectorAll('.accordion-title');
 const toggle = document.querySelectorAll('.accordion-toggle');
 
-
-
 // function openContent(){
 //         content.classList.add('toggle-on');
 // }
 // function closeContent(){
 //         content.classList.remove('toggle-on');
 // }
-
-// open.addEventListener('click', openContent);
-// close.addEventListener('click', closeContent);
 for(let i = 0; i < title.length; i++){
     title[i].addEventListener('click', function(e){
         let content = e.target.parentElement.nextElementSibling;
         content.classList.add('toggle-on');
+    });
+}
+for(let i = 0; i < toggle.length; i++){
+    toggle[i].addEventListener('click', function(e){
+        let content = e.target.parentElement.nextElementSibling;
+        content.classList.remove('toggle-on');
     });
 }
