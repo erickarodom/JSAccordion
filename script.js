919cf11bf97e87615c3cc394ecee5ba7cf3c41cc
erickarodoom <erickarodom@gmail.com>
2024-02-27 21:06:17 -0800

    work on accordian opening functionality

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-const open = document.querySelector('.accordion-title');
-const close = document.querySelector('#accordion-toggle');
-const content = document.querySelector('#accordionContainer');
-console.log(close);
+const title = document.querySelectorAll('.accordion-title');
+const toggle = document.querySelectorAll('.accordion-toggle');
 
-function openContent(){
-        
+
+
+// function openContent(){
+//         content.classList.add('toggle-on');
+// }
+// function closeContent(){
+//         content.classList.remove('toggle-on');
+// }
+
+// open.addEventListener('click', openContent);
+// close.addEventListener('click', closeContent);
+for(let i = 0; i < title.length; i++){
+    title[i].addEventListener('click', function(e){
+        let content = e.target.parentElement.nextElementSibling;
         content.classList.add('toggle-on');
+    });
 }
-function closeContent(){
-        content.classList.remove('toggle-on');
-}
-
-open.addEventListener('click', openContent);
-close.addEventListener('click', closeContent);
