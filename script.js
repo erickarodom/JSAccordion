73ffe4ab8d8e691d6adc436b24db3a669a49caa3
erickarodoom <erickarodom@gmail.com>
2024-02-29 13:14:44 -0800

    completed new element functionality, open close functionality not working on new elements yet

--- script.js
+++ script.js
@@ -6,6 +6,37 @@ const contentInput = document.getElementById('newAccordionContent');
 const submit = document.getElementById('submitButton');
 
 
+const newAccordianElement = function createAccordianElement(){
+    const newTitleContainer = document.createElement('div');
+    const newTitle = document.createElement('div');
+    const newToggle = document.createElement('div');
+   
+    // add classes to new elements
+    newTitleContainer.classList.add('accordion-title-container');
+    newTitle.classList.add('accordion-title');
+    newToggle.classList.add('accordion-toggle');
+    
+
+    //build new element
+    newTitleContainer.appendChild(newTitle);
+    newTitle.innerText = "This Is The Accordion Title Or Faq Question.";
+    newTitleContainer.appendChild(newToggle);
+    newToggle.innerText = "X";
+
+    return newTitleContainer;
+}
+
+function attachNewElement(){
+    const newContentContainer = document.createElement('div');
+    newContentContainer.classList.add('accordion-content-container');
+    newContentContainer.innerText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic ipsa reiciendis et!Lorem ipsum dolor sit amet consectetur adipisicing elit";
+    const newElement = newAccordianElement();
+    accordionContainer.appendChild(newElement);
+    accordionContainer.appendChild(newContentContainer);
+}
+
+
+
 for(let i = 0; i < title.length; i++){
     title[i].addEventListener('click', function(e){
         let content = e.target.parentElement.nextElementSibling;
@@ -20,36 +51,4 @@ for(let i = 0; i < toggle.length; i++){
 }
 
 
-const newAccordianElement = function createAccordianElement(){
-    const newTitleContainer = document.createElement('div');
-    const newTitle = document.createElement('div');
-    const newToggle = document.createElement('div');
-
-    // add classes to new elements
-    newTitleContainer.classList.add('accordion-title-container');
-    newTitle.classList.add('accordion-title');
-    newToggle.classList.add('accordion-toggle');
-
-    //build new element
-    newTitleContainer.appendChild(newTitle);
-    newTitleContainer.appendChild(newToggle);
-    newToggle.innerText = "X";
-
-    return newTitleContainer;
-}
-
-function attachNewElement(){
-    let newElement = newAccordianElement();
-    accordionContainer.appendChild(newElement);
-}
-
-
-
-
 submit.addEventListener('click', attachNewElement);
-
-
-// submit.addEventListener('click', function(){
-//     let newTitle = titleInput.value;
-//     console.log(newTitle);
-// });
