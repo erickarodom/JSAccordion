170fbe538da4f164411b5383c875b9ee3954d696
erickarodoom <erickarodom@gmail.com>
2024-02-29 11:31:53 -0800

    added variables for testing input functionality

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const title = document.querySelectorAll('.accordion-title');
 const toggle = document.querySelectorAll('.accordion-toggle');
+const accordionContainer = document.getElementById('accordianModuleContainer');
+const titleInput = document.getElementById('newAccordionTitle');
+const contentInput = document.getElementById('newAccordionContent');
+const submit = document.getElementById('submitButton');
+
 
 for(let i = 0; i < title.length; i++){
     title[i].addEventListener('click', function(e){
@@ -13,3 +18,11 @@ for(let i = 0; i < toggle.length; i++){
         content.classList.remove('toggle-on');
     });
 }
+
+
+
+
+// submit.addEventListener('click', function(){
+//     let newTitle = titleInput.value;
+//     console.log(newTitle);
+// });
